180d076cb3ff00eec99799277d0fe0b0618f1e68
Vinay Naidu <vinaynaidu@users.noreply.github.com>
2026-03-25 12:11:49 +0100

    Updated settings

--- vscode.settings.js
+++ vscode.settings.js
@@ -1,10 +1,36 @@
 {
-    "git.enabled": false,
+  "chat.autopilot.enabled": false,
+    
+  "editor.formatOnSave": true,
+  "editor.minimap.enabled": false,
+  "editor.rulers": [120, 140],
 
-    "files.trimTrailingWhitespace": true,
-    "files.insertFinalNewline": true,
-    "files.trimFinalNewlines": true,
+  "files.trimTrailingWhitespace": true,
+  "files.insertFinalNewline": true,
+  "files.trimFinalNewlines": true,
 
-    "editor.minimap.enabled": false,
-    "editor.rulers": [120, 140]
+  "git.enabled": false,
+
+  "window.commandCenter": false,
+
+  // Editor formatter
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "[javascript]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+  },
+  "[typescript]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+  },
+  "[scss]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+  },
+  "[css]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+  },
+  "[less]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+  },
+  "[html]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+  },
 }
